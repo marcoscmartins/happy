e223a4af64e5e39fa89b343208db9f32052b0937
Marcos Cardoso <dev.marcoscardoso@gmail.com>
2020-10-17 16:46:47 -0300

    Merge resolve

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -2,31 +2,21 @@ import {Request, Response} from 'express';
 import {getRepository} from 'typeorm';
 
 import Orphanage from '../models/Orphanage';
-<<<<<<< HEAD
 import orphanageView from '../views/orphanages_view';
-=======
->>>>>>> 460f7bacc0d5999f6ac6c00986b5a619647559fe
 
 export default {
     async index(request:Request, response:Response) {
         const orphanagesRepository = getRepository(Orphanage);
-<<<<<<< HEAD
         const orphanages = await orphanagesRepository.find({
             relations: ['images']
         });
 
         return response.json(orphanageView.renderMany(orphanages));
-=======
-        const orphanages = await orphanagesRepository.find();
-
-        return response.json(orphanages);
->>>>>>> 460f7bacc0d5999f6ac6c00986b5a619647559fe
     },
     async show(request:Request, response:Response) {
 
         const { id } = request.params;
         const orphanagesRepository = getRepository(Orphanage);
-<<<<<<< HEAD
         const orphanage = await orphanagesRepository.findOneOrFail(id, {
             relations: ['images']
         });
@@ -35,15 +25,6 @@ export default {
     },
     async create(request:Request, response:Response) {
         const {
-=======
-        const orphanage = await orphanagesRepository.findOneOrFail(id);
-
-        return response.json(orphanage);
-    },
-    async create(request:Request, response:Response) {
-        const {
-            id,
->>>>>>> 460f7bacc0d5999f6ac6c00986b5a619647559fe
             name,
             latitude,
             longitude,
@@ -54,7 +35,6 @@ export default {
         } = request.body;
         
         const orphanagesRepository = getRepository(Orphanage);
-<<<<<<< HEAD
         
         // hack para ligar com upload de múltiplos arquivos
         const requestImages = request.files as Express.Multer.File[];
@@ -63,23 +43,14 @@ export default {
         })
 
         const orphanage = orphanagesRepository.create({
-=======
-    
-        const orphanage = orphanagesRepository.create({
-            id,
->>>>>>> 460f7bacc0d5999f6ac6c00986b5a619647559fe
             name,
             latitude,
             longitude,
             about,
             instructions,
             opening_hours,
-<<<<<<< HEAD
             open_on_weekends,
             images
-=======
-            open_on_weekends
->>>>>>> 460f7bacc0d5999f6ac6c00986b5a619647559fe
         });
     
         await orphanagesRepository.save(orphanage);
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,4 @@
 import {Router} from 'express';
-<<<<<<< HEAD
 import multer from 'multer';
 
 import uploadConfig from './config/upload';
@@ -11,14 +10,5 @@ const upload = multer(uploadConfig);
 routes.get('/orphanages', OrphanagesController.index);
 routes.get('/orphanages/:id', OrphanagesController.show);
 routes.post('/orphanages', upload.array('images'), OrphanagesController.create);
-=======
-import OrphanagesController from './controllers/OrphanagesController';
-
-const routes = Router();
-
-routes.get('/orphanages', OrphanagesController.index);
-routes.get('/orphanages/:id', OrphanagesController.show);
-routes.post('/orphanages', OrphanagesController.create);
->>>>>>> 460f7bacc0d5999f6ac6c00986b5a619647559fe
 
 export default routes;  
